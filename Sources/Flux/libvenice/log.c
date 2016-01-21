9c5c2171e91ae77a103c069c392d212af231d9cc
Paulo Faria <paulo.faria.rl@gmail.com>
2016-01-21 15:31:27 -0200

    add libvenice

---
+++ Sources/Flux/libvenice/log.c
@@ -0,0 +1,9 @@
+//
+//  log.c
+//  Flux
+//
+//  Created by Paulo Faria on 1/21/16.
+//  Copyright © 2016 Zewo. All rights reserved.
+//
+
+#include "log.h"
---
+++ Sources/Flux/libvenice/log.h
@@ -0,0 +1,14 @@
+//
+//  log.h
+//  Flux
+//
+//  Created by Paulo Faria on 1/21/16.
+//  Copyright © 2016 Zewo. All rights reserved.
+//
+
+#ifndef log_h
+#define log_h
+
+#include <stdio.h>
+
+#endif /* log_h */
